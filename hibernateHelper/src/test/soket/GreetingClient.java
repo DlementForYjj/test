0163512fc106243b10c022f29da695c4da052c04
lenovo <lenovo@lenovo-PC>
2017-04-26 15:53:11 +0800

    调整格式

--- hibernateHelper/src/test/soket/GreetingClient.java
+++ hibernateHelper/src/test/soket/GreetingClient.java
@@ -11,20 +11,15 @@ public class GreetingClient
       int port = Integer.parseInt("6666");
       try
       {
-         System.out.println("Connecting to " + serverName
-                             + " on port " + port);
+         System.out.println("Connecting to " + serverName+ " on port " + port);
          Socket client = new Socket(serverName, port);
-         System.out.println("Just connected to "
-                      + client.getRemoteSocketAddress());
+         System.out.println("Just connected to " + client.getRemoteSocketAddress());
          OutputStream outToServer = client.getOutputStream();
-         DataOutputStream out =
-                       new DataOutputStream(outToServer);
+         DataOutputStream out =new DataOutputStream(outToServer);
  
-         out.writeUTF("Hello from "
-                      + client.getLocalSocketAddress());
+         out.writeUTF("Hello from "+ client.getLocalSocketAddress());
          InputStream inFromServer = client.getInputStream();
-         DataInputStream in =
-                        new DataInputStream(inFromServer);
+         DataInputStream in = new DataInputStream(inFromServer);
          System.out.println("Server says " + in.readUTF());
          client.close();
       }catch(IOException e)
